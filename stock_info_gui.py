5c510609327605ec327b549c6dba73af0f04eace
Makimousse <116353532+Makimousse@users.noreply.github.com>
2023-03-17 09:55:04 +0100

    reformat code into functions

--- stock_info_gui.py
+++ stock_info_gui.py
@@ -4,18 +4,28 @@ from datetime import datetime, timedelta
 import yfinance as yf
 import matplotlib.pyplot as plt
 
-# Create a Tkinter window
-window = tk.Tk()
-window.title("Stock Information")
-window.geometry("800x1000")
-
-
 # Function to get and update the historical graph with the current date_window input
 def update_historical_graph():
-    # Get the stock ticker and date window inputs from the user
     ticker = ticker_input.get()
     date_window = date_window_input.get()
-    # Get the stock price data for the specified date window
+
+    # Fetches the determined date window from get_start_and_end_date
+    start_date, end_date = get_start_and_end_date(date_window)
+    if start_date is None or end_date is None:
+        return
+    # Downloads the stock price information from the date window
+    data = yf.download(ticker, start=start_date, end=end_date)
+    # Plot the stock price data on the graph
+    ax.clear()
+    ax.plot(data.index, data['Adj Close'])
+    ax.set_title(f"{ticker} Stock Price in USD")
+    ax.set_xlabel("Date")
+    ax.set_ylabel("Price (In USD)")
+    fig.autofmt_xdate()
+    canvas.draw()
+
+# Function to fetch the date window for the historical graph
+def get_start_and_end_date(date_window):
     end_date = datetime.today() - timedelta(days=1)
     if 'y' in date_window:
         start_date = end_date - timedelta(days=365*int(date_window[:-1]))
@@ -24,19 +34,46 @@ def update_historical_graph():
     elif 'd' in date_window:
         start_date = end_date - timedelta(days=int(date_window[:-1]))
     else:
-        return
-    data = yf.download(ticker, start=start_date, end=end_date)
-    # Clear the previous graph
-    ax.clear()
-    # Plot the stock price data on the graph
-    ax.plot(data.index, data['Adj Close'])
-    ax.set_title(f"{ticker} Stock Price in USD")
-    ax.set_xlabel("Date")
-    ax.set_ylabel("Price (In USD)")
-    fig.autofmt_xdate()
-    canvas.draw()
+        return None, None
+    return start_date, end_date
 
-# Create a title widget at the top of the window
+
+def display_stock_info():
+    ticker = ticker_input.get()
+    # Uses a try and except function to manage errors
+    try:
+        # Gets the data for the ticker
+        stock = yf.Ticker(ticker)
+        old_info = stock.fast_info
+        # Reformulates the keys of the fast_info
+        new_keys = {'currency': 'Currency', 'dayHigh': 'Day High', 'dayLow': 'Day Low', 'exchange': 'Stock Exchange', 'fiftyDayAverage': '50 day average', 'lastPrice': 'Last Price', 'lastVolume': 'Last Volume', 'marketCap': 'Market cap', 'open': 'Opening price', 'previousClose': 'Previous close', 'quoteType': 'Quote type', 'regularMarketPreviousClose': 'Regular market previous close', 'shares': 'Outstanding shares', 'tenDayAverageVolume': '10 day average volume', 'threeMonthAverageVolume': '3-month average volume', 'timezone': 'Time zone', 'twoHundredDayAverage': '200 day average', 'yearChange': 'Year change', 'yearHigh': 'Year high', 'yearLow': 'Year low'}
+        new_info = {}
+        # Associates the values of fast_info with the reformulated keys of new_keys
+        for key, val in old_info.items():
+            if key in new_keys:
+                new_key = new_keys[key]
+            else:
+                new_key=key
+            new_info[new_key] = val
+        # Categorizes the keys
+        new_key_order = ['Quote type','Currency','Time zone', 'Stock Exchange','Outstanding shares','Market cap','Opening price','Previous close','Regular market previous close','Day High','Day Low','Last Price','Last Volume','10 day average volume','3-month average volume','50 day average','200 day average','Year change','Year high','Year low']
+        new_info = {new_key: new_info[new_key] for new_key in new_key_order}
+        # Clear the text widget and display the data
+        text.delete('1.0', 'end')
+        for key, value in new_info.items():
+            text.insert('end', f"{key}: {value}\n\n")
+    except:
+        # Clears the text widget and displays an error message
+        text.delete('1.0', 'end')
+        text.insert('end', "An error occurred while fetching stock information. Please try again with other values.\n")
+
+
+# Create a Tkinter window
+window = tk.Tk()
+window.title("Stock Information")
+window.geometry("800x1000")
+
+# Title for the window
 title = tk.Label(window, text="Awesome Stock app", font=("Arial", 24))
 title.pack(side="top", fill="x", pady=10)
 
@@ -52,40 +89,11 @@ date_window_label.pack()
 date_window_input = tk.Entry(window)
 date_window_input.pack()
 
-# Create a button to display the historical graph
+# Create a button to display the historical graph with the update_historical_graph function
 graph_button = tk.Button(window, text="Historical Graph", command=update_historical_graph)
 graph_button.pack()
 
-# Create a button to display the stock information
-def display_stock_info():
-    # Get the stock information for the ticker entered in the input box
-    ticker = ticker_input.get()
-    try:
-        stock = yf.Ticker(ticker)
-        old_info = stock.fast_info
-        # Clear the text widget
-        text.delete('1.0', 'end')
-        # Define the new key names in the desired order
-        new_keys = {'currency': 'Currency', 'dayHigh': 'Day High', 'dayLow': 'Day Low', 'exchange': 'Stock Exchange', 'fiftyDayAverage': '50 day average', 'lastPrice': 'Last Price', 'lastVolume': 'Last Volume', 'marketCap': 'Market cap', 'open': 'Opening price', 'previousClose': 'Previous close', 'quoteType': 'Quote type', 'regularMarketPreviousClose': 'Regular market previous close', 'shares': 'Outstanding shares', 'tenDayAverageVolume': '10 day average volume', 'threeMonthAverageVolume': '3-month average volume', 'timezone': 'Time zone', 'twoHundredDayAverage': '200 day average', 'yearChange': 'Year change', 'yearHigh': 'Year high', 'yearLow': 'Year low'}
-        # Creating new dictionary with new_key's keys and old_info's values 
-        new_info = {}
-        for key, val in old_info.items():
-            if key in new_keys:
-                new_key = new_keys[key]
-            else:
-                new_key=key
-            new_info[new_key] = val
-            
-        new_key_order = ['Quote type','Currency','Time zone', 'Stock Exchange','Outstanding shares','Market cap','Opening price','Previous close','Regular market previous close','Day High','Day Low','Last Price','Last Volume','10 day average volume','3-month average volume','50 day average','200 day average','Year change','Year high','Year low']
-        new_info = {new_key: new_info[new_key] for new_key in new_key_order}
-        for key, value in new_info.items():
-            text.insert('end', f"{key}: {value}\n\n")
-    except:
-        # Display an error message on the text widget
-        text.delete('1.0', 'end')
-        text.insert('end', "An error occurred while fetching stock information. Please try again with other values.\n")
-
-# Create a button to display the stock information
+# Create a button to display the stock information with the display_stock_info function
 info_button = tk.Button(window, text="Display Info", command=display_stock_info)
 info_button.pack()
 
@@ -96,7 +104,6 @@ text.pack()
 # Create a Quit button to close the window
 def quit_window():
     window.destroy()
-
 quit_button = tk.Button(window, text="Quit", fg='red', command=quit_window)
 quit_button.pack()
 
@@ -104,6 +111,5 @@ quit_button.pack()
 fig, ax = plt.subplots(figsize=(8,6))
 canvas = FigureCanvasTkAgg(fig, master=window)
 canvas.get_tk_widget().pack()
-
 
 window.mainloop()
---
+++ test.py
@@ -0,0 +1,115 @@
+import tkinter as tk
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from datetime import datetime, timedelta
+import yfinance as yf
+import matplotlib.pyplot as plt
+
+# Function to get and update the historical graph with the current date_window input
+def update_historical_graph():
+    ticker = ticker_input.get()
+    date_window = date_window_input.get()
+
+    # Fetches the determined date window from get_start_and_end_date
+    start_date, end_date = get_start_and_end_date(date_window)
+    if start_date is None or end_date is None:
+        return
+    # Downloads the stock price information from the date window
+    data = yf.download(ticker, start=start_date, end=end_date)
+    # Plot the stock price data on the graph
+    ax.clear()
+    ax.plot(data.index, data['Adj Close'])
+    ax.set_title(f"{ticker} Stock Price in USD")
+    ax.set_xlabel("Date")
+    ax.set_ylabel("Price (In USD)")
+    fig.autofmt_xdate()
+    canvas.draw()
+
+# Function to fetch the date window for the historical graph
+def get_start_and_end_date(date_window):
+    end_date = datetime.today() - timedelta(days=1)
+    if 'y' in date_window:
+        start_date = end_date - timedelta(days=365*int(date_window[:-1]))
+    elif 'm' in date_window:
+        start_date = end_date - timedelta(days=30*int(date_window[:-1]))
+    elif 'd' in date_window:
+        start_date = end_date - timedelta(days=int(date_window[:-1]))
+    else:
+        return None, None
+    return start_date, end_date
+
+
+def display_stock_info():
+    ticker = ticker_input.get()
+    # Uses a try and except function to manage errors
+    try:
+        # Gets the data for the ticker
+        stock = yf.Ticker(ticker)
+        old_info = stock.fast_info
+        # Reformulates the keys of the fast_info
+        new_keys = {'currency': 'Currency', 'dayHigh': 'Day High', 'dayLow': 'Day Low', 'exchange': 'Stock Exchange', 'fiftyDayAverage': '50 day average', 'lastPrice': 'Last Price', 'lastVolume': 'Last Volume', 'marketCap': 'Market cap', 'open': 'Opening price', 'previousClose': 'Previous close', 'quoteType': 'Quote type', 'regularMarketPreviousClose': 'Regular market previous close', 'shares': 'Outstanding shares', 'tenDayAverageVolume': '10 day average volume', 'threeMonthAverageVolume': '3-month average volume', 'timezone': 'Time zone', 'twoHundredDayAverage': '200 day average', 'yearChange': 'Year change', 'yearHigh': 'Year high', 'yearLow': 'Year low'}
+        new_info = {}
+        # Associates the values of fast_info with the reformulated keys of new_keys
+        for key, val in old_info.items():
+            if key in new_keys:
+                new_key = new_keys[key]
+            else:
+                new_key=key
+            new_info[new_key] = val
+        # Categorizes the keys
+        new_key_order = ['Quote type','Currency','Time zone', 'Stock Exchange','Outstanding shares','Market cap','Opening price','Previous close','Regular market previous close','Day High','Day Low','Last Price','Last Volume','10 day average volume','3-month average volume','50 day average','200 day average','Year change','Year high','Year low']
+        new_info = {new_key: new_info[new_key] for new_key in new_key_order}
+        # Clear the text widget and display the data
+        text.delete('1.0', 'end')
+        for key, value in new_info.items():
+            text.insert('end', f"{key}: {value}\n\n")
+    except:
+        # Clears the text widget and displays an error message
+        text.delete('1.0', 'end')
+        text.insert('end', "An error occurred while fetching stock information. Please try again with other values.\n")
+
+
+# Create a Tkinter window
+window = tk.Tk()
+window.title("Stock Information")
+window.geometry("800x1000")
+
+# Title for the window
+title = tk.Label(window, text="Awesome Stock app", font=("Arial", 24))
+title.pack(side="top", fill="x", pady=10)
+
+# Create a label and input box for the ticker
+ticker_label = tk.Label(window, text="Enter ticker:")
+ticker_label.pack()
+ticker_input = tk.Entry(window)
+ticker_input.pack()
+
+# Create a label and input box for the date window
+date_window_label = tk.Label(window, text="Enter date window ('1y', '1m', or '1d'):")
+date_window_label.pack()
+date_window_input = tk.Entry(window)
+date_window_input.pack()
+
+# Create a button to display the historical graph with the update_historical_graph function
+graph_button = tk.Button(window, text="Historical Graph", command=update_historical_graph)
+graph_button.pack()
+
+# Create a button to display the stock information with the display_stock_info function
+info_button = tk.Button(window, text="Display Info", command=display_stock_info)
+info_button.pack()
+
+# Create a text widget to display the stock information
+text = tk.Text(window)
+text.pack()
+
+# Create a Quit button to close the window
+def quit_window():
+    window.destroy()
+quit_button = tk.Button(window, text="Quit", fg='red', command=quit_window)
+quit_button.pack()
+
+# Create a FigureCanvasTkAgg object for displaying the historical graph
+fig, ax = plt.subplots(figsize=(8,6))
+canvas = FigureCanvasTkAgg(fig, master=window)
+canvas.get_tk_widget().pack()
+
+window.mainloop()
